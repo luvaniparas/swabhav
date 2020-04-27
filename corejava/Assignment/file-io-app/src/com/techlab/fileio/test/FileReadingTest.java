55533e284294c695bbe0b27ed0b32c6865e4093e
Paras Luvani <luvaniparas5@gmail.com>
2020-04-27 22:05:35 +0530

    Day4 Assignment FileIO

--- corejava/Assignment/file-io-app/src/com/techlab/fileio/test/FileReadingTest.java
+++ corejava/Assignment/file-io-app/src/com/techlab/fileio/test/FileReadingTest.java
@@ -2,6 +2,7 @@ package com.techlab.fileio.test;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.util.Scanner;
 
 public class FileReadingTest {
@@ -13,6 +14,8 @@ public class FileReadingTest {
 		FileReadingTest cobj = new FileReadingTest();
 		File fobj = new File(
 				"C:\\swabhav\\corejava\\Assignment\\file-io-app\\src\\com\\techlab\\fileio\\test\\logTest_app.csv");
+		// FileWriter myWriter = new
+		// FileWriter("C:\\swabhav\\corejava\\Assignment\\file-io-app\\src\\com\\techlab\\fileio\\test\\logTest_app.csv");
 		try {
 			sc = new Scanner(fobj);
 			cobj.printLog(fobj);
@@ -21,7 +24,7 @@ public class FileReadingTest {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void printLog(File fobj) {
 		try {
 			sc = new Scanner(fobj);
@@ -29,14 +32,24 @@ public class FileReadingTest {
 			System.out.println("Can not Open file");
 			e.printStackTrace();
 		}
-		//String line = "";
+		String str = "null" ;
+		Boolean blr = true ;
+		String replaceString ;
 		
-		sc.useDelimiter(",");    
-		while (sc.hasNext())   
-		{  
-		System.out.print(sc.next());  
-		}   
+		while (sc.hasNext()) {
+			/*if (sc.nextLine().length() != 1) {
+				System.out.println(sc.next());
+			}*/
+			String line = sc.nextLine();
+			 if(line.contains(str) == blr ) {
+				replaceString=line.replace(str,"Unspecified");  
+				System.out.println(replaceString); 
+			}
+			 else
+			 {
+				 System.out.println(line);	 
+			 }
+		}
 		sc.close();
+	}
 }
-}
-
