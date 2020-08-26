fa85d433c7d6d8164c19ca71a938cbd9d7f54013
Paras Luvani <luvaniparas5@gmail.com>
2020-08-27 03:49:38 +0530

    Numbers API

---
+++ JavaScript/JQuery/Assignment/Numbers-API/NumbersJS.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+    var i = 1;
+    var number ;
+    
+    //On Body Load
+    $("body").on("load",loadData());
+    function loadData(){
+        var  key ;
+        for (key in window.localStorage){
+            if(window.localStorage.hasOwnProperty(key)){
+                  let time =  localStorage.getItem(key);
+                  key = key.split(',');
+                  createNode(key,time);
+            }           
+        }
+    }
+
+    //LocalStorage
+    $("#b1").on("click",function(){
+        const time = moment().format('MM D YYYY hh:mm:ss');
+        number = $("#t1").val();
+        localStorage.setItem(number,time); 
+        createNode(number,time);
+    });
+
+    //Dynamic Creation
+    function createNode(number,time){ 
+        if(number == 0 ){
+            $("#t1").css("borderColor","red");
+            $("#s1").text("Task must be filled out");
+            $("#s1").css("color","red");
+        }else{
+            //localStorage 
+            rowId = "R"+(i++); 
+            let timeDifference = calculateTimeDifference(time);
+
+            //Dynamic Creation
+            $.get('http://numbersapi.com/'+number+'/trivia', function(data) {
+                $("#nList").append("<p id ="+rowId+">"+"</p>");
+                $("<label>"+timeDifference+"</label>").appendTo("#"+rowId);    
+                $("<label>"+data+"</label>").appendTo("#"+rowId);
+                $("<br>").appendTo("#"+rowId);
+            });
+            }
+    } 
+    
+    //TimeDifference
+    function calculateTimeDifference(time) {
+        let taskAddedTime = moment(time);
+        let currentTime = moment(moment().format('MM D YYYY hh:mm:ss '));
+        let compare = moment.duration(taskAddedTime.diff(currentTime));
+        return compare.humanize(true);
+    }
+});
+    
