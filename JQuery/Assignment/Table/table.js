919eba0080127686993f5eb064202df648bc80e5
Paras Luvani <luvaniparas5@gmail.com>
2020-09-02 22:59:26 +0530

    AJAX request

---
+++ JQuery/Assignment/Table/table.js
@@ -0,0 +1,28 @@
+$(document).ready(function () { 
+    // FETCHING DATA FROM JSON FILE 
+    $.getJSON("http://localhost:3000/Students",function (data) { 
+        var student = ''; 
+
+        // ITERATING THROUGH OBJECTS 
+        $.each(data, function (key, value) { 
+
+            //CONSTRUCTION OF ROWS HAVING 
+            // DATA FROM JSON OBJECT 
+            student += '<tr>'; 
+            student += '<td>' + value.name + '</td>'; 
+
+            student += '<td>' + value.id + '</td>'; 
+
+            student += '<td>' + value.age + '</td>'; 
+
+            student += '<td>' + value.gender + '</td>';
+
+            student += '<td>' + value.address + '</td>'; 
+
+            student += '</tr>'; 
+        }); 
+          
+        //INSERTING ROWS INTO TABLE  
+        $('#table').append(student); 
+    }); 
+}); 
---
+++ JQuery/Table-AjaxCall.html/TableAjaxCall.js
@@ -0,0 +1,82 @@
+$(document).ready(function(){
+
+    var rollNumber,name,age,email,date,gender;
+
+    $("#get").on("click",getRequest);
+    $("#post").on("click",postRequest);
+    $("#submit").on("click",fetchFormData);
+
+function getRequest(){
+
+    $("#Table").show();
+
+    $.ajax({
+        type:"GET",
+        url:"http://gsmktg.azurewebsites.net/api/v1/techlabs/test/students",
+        dataType:"json",
+        success: function (data){            
+            var student = ''; 
+        
+            $.each(data, function (key, value) { 
+     
+                student += '<tr>'; 
+                student += '<td>' + value.id + '</td>'; 
+    
+                student += '<td>' + value.rollNo + '</td>'; 
+    
+                student += '<td>' + value.name + '</td>'; 
+    
+                student += '<td>' + value.age + '</td>';
+    
+                student += '<td>' + value.email + '</td>'; 
+                
+                student += '<td>' + value.date + '</td>';
+
+                student += '<td>' + value.isMale + '</td>';
+
+                student += '</tr>'; 
+            });             
+            $("#Table").append(student);
+        }
+    });
+}
+
+function postRequest(){
+
+    $("#Table").hide();
+    $("#form").show();
+    var booleanGender = false ;
+    if(gender == "male"){
+        booleanGender = true ;
+    }else{
+        booleanGender = false ;
+    }
+
+    $.ajax({
+        type:"POST",
+        url:"http://gsmktg.azurewebsites.net/api/v1/techlabs/test/students",
+        data: { 
+            "rollNo": rollNumber ,
+            "name": name,
+            "age": age,
+            "email": email,
+            "date": date,
+            "isMale": booleanGender 
+            },
+        dataType:"json",
+        });
+    getRequest();
+}
+
+function fetchFormData(){
+    
+    rollNumber = $("#rollNumber").val();
+    name = $("#name").val();
+    age = $("#age").val();
+    email = $("#email").val();
+    date = $("#date").val();
+    gender = $("input[name='gender']:checked").val();
+    
+    postRequest();
+}
+});
