0383093874e89f74b0b72cd4f58564ed8a087f50
Paras Luvani <luvaniparas5@gmail.com>
2020-10-01 13:33:19 +0530

    Day 49 NumbersAPI AJS

---
+++ AngularJS/Assignment/NumbersAPI/numbers.js
@@ -0,0 +1,56 @@
+    var app = angular.module("myapp",[]);
+    app.controller("cntrl",function($scope,$http){
+        $scope.number ;
+        $scope.numbersTrivia = [];
+        
+        //On Body Load
+        $scope.load = function(){
+            var  key ;
+            for (key in window.localStorage){
+                if(window.localStorage.hasOwnProperty(key)){
+                    let time =  localStorage.getItem(key);
+                    key = key.split(',');
+                    console.log(key+" "+time);
+                    $scope.createNode(key,time);
+                }           
+            }
+        }
+       
+        //TimeDifference
+        $scope.calculateTimeDifference = function(time){
+            let taskAddedTime = moment(time);
+            let currentTime = moment(moment().format('MM D YYYY hh:mm:ss'));
+            let compare = moment.duration(taskAddedTime.diff(currentTime));
+            return compare.humanize(true);
+        }
+
+        //localStorage
+        $scope.getUserData = function(){
+            var time = moment().format('MM D YYYY hh:mm:ss');
+            var number = parseInt($scope.number);
+            
+            localStorage.setItem(number,time); 
+            $scope.createNode(number,time);
+        }
+
+        //fetchData request
+        $scope.createNode = function(number,time){
+            $http({
+                    method : "GET",
+                    url : 'http://numbersapi.com/'+number+'/trivia'
+            }).then(function mySuccess(response) {
+                var myWelcome = response.data;
+                console.log("mySuccess : " + myWelcome);
+
+                $scope.numbersTrivia.push({
+                    'number': number,
+                    'trivia': response.data,
+                    'MomentTime': $scope.calculateTimeDifference(time),
+                });
+              },function myError(response) {
+                var res = response.statusText;
+                console.log("Eror : " + res);
+            });
+        }    
+    });
+
