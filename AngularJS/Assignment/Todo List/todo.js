dab453de8fefdf89dfa482a768cbbaea2a24dc50
Paras Luvani <luvaniparas5@gmail.com>
2020-10-01 19:43:07 +0530

    Day 50 To-Do Update

--- AngularJS/Assignment/Todo List/todo.js
+++ AngularJS/Assignment/Todo List/todo.js
@@ -13,44 +13,24 @@ app.controller("cntrl",function($scope,$filter){
         }
     }
 
-    $scope.addTask = function(){
-        //If null task 
+    $scope.addTask = function() {
+        //If null task
         if($scope.taskTx == "" || $scope.taskTx == undefined ){
             console.log("Empty");
         }else{
             $scope.tasks.push({'taskMessage' : $scope.taskTx,'status' : false});
 
             localStorage['taskList'] = JSON.stringify($scope.tasks);
-        } 
-        
+        }
+    }
+
     $scope.remove = function(index){
         console.log("Inside remove");
-        $scope.tasks.splice(index,1);
+        $scope.tasks.splice(index,1);   
     }
-    
-    $scope.update = function(){
-        console.log("Insdie Update");
-        contenteditable="true" ; 
 
-
+    $scope.update = function(event){
+        event.target.contentEditable = event.target.contentEditable == "false" ? "true" : "false" ;
     }
-    /*
-    <th>Update</th>
-    <td><button ng-click="update($index.t.taskMessage)"> Update </button></td>
-    $scope.update = function(task){
-        console.log("Inside Update : "+task);
-        
-        $scope.updateTask = task;
 
-        $('.mainForm').append(
-            "<span class='update-item'>" +
-            "<input type='text' class='update-input'/>" +
-            "<button class='update-btn'>Update</button>" +
-            "</span>"
-        );
-        $('.update-input').val(task);
-    }
-    */    
-    };   
 });
-
