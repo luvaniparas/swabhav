d13cca86dcc8bc5faed2cf5bd9db2a4d07f4d54a
Paras Luvani <luvaniparas5@gmail.com>
2020-10-31 01:39:58 +0530

    Day 55 Angular JS

--- AngularJS/Assignment/SPA Bike/spa.js
+++ AngularJS/Assignment/SPA Bike/spa.js
@@ -5,25 +5,40 @@ app.config(function($routeProvider) {
     
     .when("/bike", {
         templateUrl : "./bike.html",
-        controller : "cntrl"
+        controller : "bike"
     })
     .when("/bajaj", {
       templateUrl : "./bajaj.html",
-      controller : "cntrl"
+      controller : "bajaj"
     })
     .when("/yamaha", {
       templateUrl : "./yamaha.html",
-      controller : "cntrl"
+      controller : "yamaha"
     })
     .when("/Honda", {
       templateUrl : "./Honda.html",
-      controller : "cntrl"
+      controller : "honda"
     })
     .otherwise({
       redirectTo: './bike.html'
     })
   });
 
-app.controller("cntrl",function($scope,$location) {
-    console.log($location.path());
+app.controller("bike",function() {
+    console.log("Inside bike controller");
+});
+
+app.controller("bajaj",function($scope) {
+  $scope.name = "Bajaj"
+  console.log("Inside Bajaj controller");
+});
+
+app.controller("yamaha",function($scope) {
+  $scope.name = "yamaha"
+  console.log("Inside Yamaha controller");
+});
+
+app.controller("honda",function($scope) {
+  $scope.name = "Honda"
+  console.log("Inside Honda controller");
 });
